ad9c91e3fdb3a9e12e38b8c6a0387cbafbfbc9bf
xicilion <lion@9465.net>
2018-02-18 19:25:25 +0800

    1

--- .travis/build.sh
+++ .travis/build.sh
@@ -6,7 +6,11 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     sh build -j2 ${ARCH} ci
 else
     echo "start docker build..."
-    DIR=`pwd`;sudo docker run -it -v ${DIR}:/home/ci fibjs/build-env:latest /bin/sh -c "cd /home/ci; sh build -j2 ${ARCH} ci"
+    if [[ "${TARGET}" == "alpine" ]]; then
+        DIR=`pwd`;sudo docker run -it -v ${DIR}:/home/ci xicilion/fibjs-build-alpine:latest /bin/sh -c "cd /home/ci; sh build -j2 ${ARCH} ci"
+    else
+        DIR=`pwd`;sudo docker run -it -v ${DIR}:/home/ci fibjs/build-env:latest /bin/sh -c "cd /home/ci; sh build -j2 ${ARCH} ci"
+    fi
 fi
 
 exit 0;
