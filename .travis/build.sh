07bbf86969f0be57087433da87cec60e52dd2b51
xicilion <lion@9465.net>
2018-02-18 19:16:23 +0800

    test build

--- .travis/build.sh
+++ .travis/build.sh
@@ -2,11 +2,11 @@
 
 set -ev
 
-if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
+if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+    sh build -j2 ${ARCH} ci
+else
     echo "start docker build..."
     DIR=`pwd`;sudo docker run -it -v ${DIR}:/home/ci fibjs/build-env:latest /bin/sh -c "cd /home/ci; sh build -j2 ${ARCH} ci"
-else
-    sh build -j2 ${ARCH} ci
 fi
 
 exit 0;
